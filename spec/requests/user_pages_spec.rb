346da58737911a588fbf99297f414583740e6b47
Strata <nagashree.aswath@strata.co.in>
2013-02-12 10:42:54 +0530

    finished sign-in & sign-out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,10 +35,10 @@ describe "profile page" do
         fill_in "Email",        :with=> "user@example.com"
         fill_in "Password",     :with=> "foobar"
         fill_in "Confirmation", :with=> "foobar"
-      end
-
+        end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+        
       end
     end
   end
